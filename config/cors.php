2013daf357d793d3ecf203fac661a83bda517d2e
TUR PC <alhrabi.tur1@gmail.com>
2022-07-21 11:21:00 +0300

    fix issues

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tur-august.herokuapp.com', 'http://tur-august.herokuapp.com', '*'],
 
     'allowed_origins_patterns' => ['*'],
 
